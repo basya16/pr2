324eb09f9162d15c70fa85456593812f3912b220
basya16 <milenamilana@icloud.com>
2025-03-10 16:09:17 +0200

    Update hw03.rs

--- src/homework/hw03.rs
+++ src/homework/hw03.rs
@@ -1,1 +1,23 @@
-шо09л0
+fn main() {
+    const WIDTH: usize = 20;
+    const HEIGHT: usize = 10;
+
+    for i in 0..WIDTH {
+        print!("*");
+    }
+    println!();
+
+    for _ in 0..HEIGHT - 2 {
+        print!("*");
+        for _ in 0..WIDTH - 2 {
+            print!(" ");
+        }
+        println!("*");
+    }
+
+    for i in 0..WIDTH {
+        print!("*");
+    }
+    println!();
+}
+
