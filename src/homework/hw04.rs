5033d91c08e91b3e838e6cfd58df1afc37ac6364
basya16 <milenamilana@icloud.com>
2025-03-19 19:13:25 +0200

    Update hw04.rs

---
+++ src/homework/hw04.rs
@@ -0,0 +1,36 @@
+const W: u32 = 11;
+const H: u32 = 5;
+fn main() { 
+    for y in 0..H {
+      let spaces = H - y - 1;
+      let stars = 2 * y + 1;
+
+      for _ in 0..spaces {
+            print!(" ");
+        }
+      for _ in 0..stars {
+            println!("*");
+        }
+
+      println();
+    }
+   for y in (0..H - 1).rev() {
+     let spaces = H - y - 1;
+     let stars = 2 * y + 1;
+
+     for _ in 0..spaces {
+            print!(" ");
+        }
+     for _ in 0..stars {
+            print!("*");
+        }
+     println!();
+    }
+}
+
+    
+
+      
+
+
+  
