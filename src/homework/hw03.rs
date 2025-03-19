c7123f7ad4a3c686a368e69ed429e27cf51b623a
basya16 <milenamilana@icloud.com>
2025-03-19 18:36:46 +0200

    Update hw03.rs

--- src/homework/hw03.rs
+++ src/homework/hw03.rs
@@ -1,23 +1,16 @@
+const W: u32 = 30; //це ширина конверта
+const H: u32 = 15; //висота
+
 fn main() {
-    const WIDTH: usize = 20;
-    const HEIGHT: usize = 10;
-
-    for i in 0..WIDTH {
-        print!("*");
-    }
-    println!();
-
-    for _ in 0..HEIGHT - 2 {
-        print!("*");
-        for _ in 0..WIDTH - 2 {
-            print!(" ");
+    for y in 0..H {
+        for x in 0..W {
+            let border: bool = y == 0 || y == H - 1 || x == 0 || x == W - 1;
+            let diagonal: bool = x == 2 * y;
+            let diagonal2: bool = x == W - 1 - 2 * y;
+            let show: bool =  border || diagonal || diagonal2;
+            let sym: &str =  if show { "*" } else { " "};
+            print!("{}", sym);
         }
-        println!("*");
-    }
-
-    for i in 0..WIDTH {
-        print!("*");
-    }
-    println!();
-}
-
+        printl!();
+    } 
+}    
