ad4ad8c5a7624c9074544efcc126836309a2f631
basya16 <milenamilana@icloud.com>
2025-04-14 17:09:07 +0300

    Update hw03.rs

--- src/homework/hw03.rs
+++ src/homework/hw03.rs
@@ -1,13 +1,16 @@
-const W: u32 = 30; //це ширина конверта
-const H: u32 = 15; //висота
+const W: u32 = 28; //це ширина конверта
+const H: u32 = 10; //висота
 
 fn main() {
     for y in 0..H {
         for x in 0..W {
-            let border: bool = y == 0 || y == H - 1 || x == 0 || x == W - 1;
-            let diagonal: bool = x == 2 * y;
-            let diagonal2: bool = x == W - 1 - 2 * y;
-            let show: bool =  border || diagonal || diagonal2;
+            let border = y == 0 || y == H - 1 || x == 0 || x == W - 1;
+            let diag1_x = ((W - 1) as f32 * (y as f32) / (H - 1) as f32).round() as u32;
+            let diag2_x = ((W - 1) as f32 * ((H - 1 - y) as f32) / (H - 1) as f32).round() as u32;
+            let diagonal = x == diag1_x;
+            let diagonal2 = x == diag2_x;
+           
+            let show =  border || diagonal || diagonal2;
             let sym: &str =  if show { "*" } else { " "};
             print!("{}", sym);
         }
